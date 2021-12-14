4e49bb2edf65cc01f2c874bba6db44f23b05f9b5
Matthew Sherman <matt5064@yahoo.com>
2021-12-14 12:49:55 -0700

    USERS_CRUD

---
+++ flask_app/models/user_model.py
@@ -0,0 +1,43 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+class Users:
+    def __init__( self , data ):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM users;"
+        results = connectToMySQL('users').query_db(query)
+        users = []
+        for user in results:
+            users.append( cls(user) )
+        return users
+
+    @classmethod
+    def get_one(cls, data:dict):
+        query = "SELECT * FROM users WHERE id = %(id)s;"
+        results = connectToMySQL('users').query_db(query, data)
+        if not results:
+            return False
+        return cls(results[0])
+
+    @classmethod
+    def save(cls, data):
+        query = "INSERT INTO users (first_name, last_name, email) VALUES (%(first_name)s, %(last_name)s, %(email)s);"      
+        result = connectToMySQL('users').query_db(query,data)
+        return result
+
+    @classmethod
+    def update(cls, data):
+        query = "UPDATE users SET first_name=%(first_name)s, last_name=%(last_name)s, email=%(email)s WHERE id=%(id)s"
+        result = connectToMySQL('users').query_db(query,data)
+        return result
+
+    @classmethod
+    def delete_one(cls, data):
+        query = 'DELETE FROM users WHERE id = %(id)s'
+        return connectToMySQL('users').query_db(query, data)
